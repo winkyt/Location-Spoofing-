ffe3c5e4ca8d89e8893570d7d301097befa13332
bo <jakobzhao@gmail.com>
2017-05-29 14:18:35 -0700

    0.0.5

--- js/graphic.js
+++ js/graphic.js
@@ -1,55 +1,55 @@
 'use strict';
 
-//EXAMPLE ONE
-var sales = [
-    { product: 'Snickers',  count: 12 },
-    { product: 'Twix',  count: 30 },
-    { product: 'Pez', count: 9 },
-    { product: 'Nerds', count: 19 },
-    { product: 'Rollos', count: 10 },
-    { product: 'Altoids', count: 5 },
-    { product: 'N/A', count: 15 },
-];
-//set up the angles of the pie chart using the pie layout helper
-var pie = d3.layout.pie()
-    .value(function(d) { return d.count })
-
-//connect our data to the slices
-var slices = pie(sales);
-
-//size of the pie chart
-var arc = d3.svg.arc()
-    .innerRadius(0)
-    .outerRadius(100);
-
-// another helper that returns a color based on an ID, category10
-var color = d3.scale.category10();
-
-
-var svg = d3.select('svg.pie');
-var g = svg.append('g')
-    .attr('transform', 'translate(200, 100)')
-
-g.selectAll('path.slice')
-    .data(slices)
-    .enter()
-    .append('path')
-    .attr('class', 'slice')
-    .attr('d', arc)
-    .attr('fill', function(d) {
-        return color(d.data.product);
-    });
-
-// building a legend
-svg.append('g')
-    .attr('class', 'legend')
-    .selectAll('text')
-    .data(slices)
-    .enter()
-    .append('text')
-    .text(function(d) { return '• ' + d.data.product + ' (' + d.data.count + ')'; })
-    .attr('fill', function(d) { return color(d.data.product); })
-    .attr('y', function(d, i) { return 20 * (i + 1); })
+// //EXAMPLE ONE
+// var sales = [
+//     { product: 'Snickers',  count: 12 },
+//     { product: 'Twix',  count: 30 },
+//     { product: 'Pez', count: 9 },
+//     { product: 'Nerds', count: 19 },
+//     { product: 'Rollos', count: 10 },
+//     { product: 'Altoids', count: 5 },
+//     { product: 'N/A', count: 15 },
+// ];
+// //set up the angles of the pie chart using the pie layout helper
+// var pie = d3.layout.pie()
+//     .value(function(d) { return d.count })
+//
+// //connect our data to the slices
+// var slices = pie(sales);
+//
+// //size of the pie chart
+// var arc = d3.svg.arc()
+//     .innerRadius(0)
+//     .outerRadius(100);
+//
+// // another helper that returns a color based on an ID, category10
+// var color = d3.scale.category10();
+//
+//
+// var svg = d3.select('svg.pie');
+// var g = svg.append('g')
+//     .attr('transform', 'translate(200, 100)')
+//
+// g.selectAll('path.slice')
+//     .data(slices)
+//     .enter()
+//     .append('path')
+//     .attr('class', 'slice')
+//     .attr('d', arc)
+//     .attr('fill', function(d) {
+//         return color(d.data.product);
+//     });
+//
+// // building a legend
+// svg.append('g')
+//     .attr('class', 'legend')
+//     .selectAll('text')
+//     .data(slices)
+//     .enter()
+//     .append('text')
+//     .text(function(d) { return '• ' + d.data.product + ' (' + d.data.count + ')'; })
+//     .attr('fill', function(d) { return color(d.data.product); })
+//     .attr('y', function(d, i) { return 20 * (i + 1); })
 
 
 //EXAMPLE 2
